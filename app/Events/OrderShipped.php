f243d2c9a48f54e7aa8b8d2762668770dcf9d7aa
khanhn-1040 <nguyen.khanh@sun-asterisk.com>
2021-07-04 22:46:49 +0700

    add laravel echo

--- app/Events/OrderShipped.php
+++ app/Events/OrderShipped.php
@@ -7,10 +7,12 @@ use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
 
-class OrderShipped
+class OrderShipped implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
@@ -21,16 +23,10 @@ class OrderShipped
      */
     public function __construct()
     {
-        //
     }
 
-    /**
-     * Get the channels the event should broadcast on.
-     *
-     * @return \Illuminate\Broadcasting\Channel|array
-     */
     public function broadcastOn()
     {
-        return new PrivateChannel('channel-name');
+        return new Channel('channel-test');
     }
 }
